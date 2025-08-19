ca2d59f1609c42e377bc2ea9851e174f65ffce0b
unknown <lillianli.studio@gmail.com>
2025-08-19 20:04:21 +0100

    commit c

--- script.js
+++ script.js
@@ -13,4 +13,23 @@ window.addEventListener('DOMContentLoaded', function() {
         heroBanner.classList.add('hide');
         fixedLogo.classList.add('show');
     }, 3000);
+
+    // Nav button morph animation
+    const navBtn = document.getElementById('nav-toggle');
+    if (navBtn) {
+        navBtn.addEventListener('mouseenter', () => {
+            gsap.to("#navPath", {
+                morphSVG: "#navPathHover",
+                duration: 0.5,
+                fill: "#a259ff"
+            });
+        });
+        navBtn.addEventListener('mouseleave', () => {
+            gsap.to("#navPath", {
+                morphSVG: {shape: "#navPath"},
+                duration: 0.5,
+                fill: "#745E5E"
+            });
+        });
+    }
 });
